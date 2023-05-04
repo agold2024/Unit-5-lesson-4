fe4cff87169ed4ca98e6a455fef106e7cbdf2363
Allison Gold <agold2024@kehillahstudent.org>
2023-05-04 11:09:18 -0700

    save

---
+++ q1.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+url = "https://en.wikipedia.org/wiki/Minnesota"
+dfs= pd.read_html(
+    #df stands for data frame
+    url, 
+    match= "Historical population"
+    )
+df = dfs[0]
+df = df.iloc[0:-2]
+df=df[["Census", "Pop."]].astype("int")
+print(df.info())
+df.plot(x= "Census", y= "Pop.")
+plt.show
+#plt.show lets use see the output
